271601bbcb67ecfdca17faaecf8617b930813e10
fitabe <fitabe@example.com>
2023-05-27 13:31:41 -0500

    port and env updated

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 // const pool = mysql.createPool({
 //     host: process.env.DB_HOST,
 //     user: process.env.DB_USER,
@@ -8,7 +8,8 @@ require('dotenv').config();
 //     connectionLimit: 10
 // })
 
-// const pool = mysql.createConnection(process.env.DATABASE_URL)
+const pool = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
 
 let registration = `CREATE TABLE if not exists registration(
     user_id int auto_increment,
@@ -21,7 +22,7 @@ let profile = `CREATE TABLE if not exists profile(
     user_profile_id int auto_increment,
     user_id int not null,
     first_name varchar(255) not null,
-    last_name varchar(255) not null,        
+    last_name varchar(255) not null,
     PRIMARY KEY (user_profile_id)
 )`;
 let question = `CREATE TABLE if not exists question(
@@ -44,20 +45,87 @@ let answer = `CREATE TABLE if not exists answer(
     PRIMARY KEY (answer_id)
 )`;
 pool.query(registration, (err, results) => {
-    if (err) throw err;
-    console.log('registration table created');
-})
+  if (err) throw err;
+  console.log("registration table created");
+});
 pool.query(profile, (err, results) => {
-    if (err) throw err;
-    console.log('profile table created');
-})
+  if (err) throw err;
+  console.log("profile table created");
+});
 pool.query(question, (err, results2) => {
-    if (err) throw err;
-    console.log('question table created');
-})
+  if (err) throw err;
+  console.log("question table created");
+});
 pool.query(answer, (err, results2) => {
-    if (err) throw err;
-    console.log('answer table created');
-})
+  if (err) throw err;
+  console.log("answer table created");
+});
 
 module.exports = pool;
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   connectionLimit: 10,
+// });
+// pool.getConnection(function (err, connection) {
+//   console.log("Database connected");
+// });
+
+// let registration = `CREATE TABLE if not exists registration(
+//     user_id int auto_increment,
+//     user_name varchar(255) not null,
+//     user_email varchar(255) not null,
+//     user_password varchar(255) not null,
+//     PRIMARY KEY (user_id)
+// )`;
+// let profile = `CREATE TABLE if not exists profile(
+//     user_profile_id int auto_increment,
+//     user_id int not null,
+//     first_name varchar(255) not null,
+//     last_name varchar(255) not null,
+//     PRIMARY KEY (user_profile_id),
+//     FOREIGN KEY (user_id) REFERENCES registration(user_id)
+// )`;
+// let question = `CREATE TABLE if not exists question(
+//     question_id int auto_increment,
+//     user_id int not null,
+//     title varchar(200) not null,
+//     question text(65535) not null,
+//     time DateTime not null,
+//     PRIMARY KEY (question_id),
+//     FOREIGN KEY (user_id) REFERENCES registration(user_id)
+//     )`;
+// let answer = `CREATE TABLE if not exists answer(
+//     answer_id int auto_increment,
+//     user_id int not null,
+//     question_id int not null,
+//     answer text(65535) not null,
+//     time DateTime not null,
+//     PRIMARY KEY (answer_id),
+//     FOREIGN KEY (user_id) REFERENCES registration(user_id),
+//     FOREIGN KEY (question_id) REFERENCES question(question_id)
+//     )`;
+
+// pool.query(registration, (err, results) => {
+//   if (err) throw err;
+//   console.log("registration table created");
+// });
+// pool.query(profile, (err, results) => {
+//   if (err) throw err;
+//   console.log("profile table created");
+// });
+// pool.query(question, (err, results) => {
+//   if (err) throw err;
+//   console.log("question table created");
+// });
+// pool.query(answer, (err, results) => {
+//   if (err) throw err;
+//   console.log("answer table created");
+// });
+
+// module.exports = pool;
